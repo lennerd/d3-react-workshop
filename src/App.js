1c6df0f580a0c5477a661d39fe0f0fe1c19e8b79
Lennart Hildebrandt <hello@lennerd.com>
2018-12-03 19:44:15 +0100

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from 'd3';
+import { createIncomeData } from './data';
+import LineGraph from './LineGraph';
 
 class App extends Component {
+  state = {
+    curves: [
+      d3.curveLinear,
+      d3.curveMonotoneX,
+      d3.curveBasis,
+      d3.curveStepAfter,
+    ],
+    data: createIncomeData(),
+  };
+
   render() {
+    const { curves, data } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {curves.map((curve, index) => (
+          <LineGraph
+            key={index}
+            width="400"
+            height="400"
+            curve={curve}
+            data={data}
+          />
+        ))}
+        <div style={{ textAlign: 'center' }}>
+          <button onClick={this.handleClick} style={{ fontSize: '2em' }}>Shuffle</button>
+        </div>
       </div>
     );
+  }
+
+  handleClick = () => {
+    this.setState({
+      data: createIncomeData(),
+    });
   }
 }
 
---
+++ src/LineGraph.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+function LineGraph(props) {
+  const { curve, data, width, height } = props;
+
+  const margin = {
+    top: 15,
+    right: 15,
+    bottom: 30,
+    left: 30,
+  };
+
+  const x = d3
+    .scaleLinear()
+    .domain(d3.extent(data, d => d.year))
+    .range([margin.left, width - margin.right])
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, d => d.income)])
+    .nice()
+    .range([height - margin.bottom, margin.top]);
+
+  const line = d3
+    .line()
+    .curve(curve)
+    .x(d => x(d.year))
+    .y(d => y(d.income));
+
+  const xAxis = d3.axisBottom(x)
+    .tickFormat(d3.format("d"));
+
+  const yAxis = d3.axisLeft(y);
+
+  return (
+    <svg width={width} height={height}>
+      <path d={line(data)} stroke="black" strokeWidth="1" fill="none" />
+      <g ref={call(xAxis)} transform={`translate(0, ${height - margin.bottom})`} />
+      <g ref={call(yAxis)} transform={`translate(${margin.left}, 0)`} />
+    </svg>
+  );
+}
+
+function call(callback) {
+  return ref => d3.select(ref).call(callback);
+} 
+
+export default LineGraph;
